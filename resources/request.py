df879932d26c250db6c78c5eb32086748ec4b570
Erick-Faster <erick.fasterra@gmail.com>
2020-04-26 17:45:22 -0300

    quest put

--- resources/request.py
+++ resources/request.py
@@ -2,6 +2,34 @@ from flask_restful import Resource, reqparse
 from models.request import RequestModel
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
+_request_parser = reqparse.RequestParser()
+_request_parser.add_argument('name',
+    type=str,
+    required=True,
+    help="This field cannot be left blank!"
+)
+_request_parser.add_argument('address',
+    type=str,
+    required=True,
+    help="This field cannot be left blank!"
+)
+_request_parser.add_argument('author',
+    type=str,
+    required=True,
+    help="Every request needs a request_id"
+)
+_request_parser.add_argument('accepted',
+    type=str,
+    required=True,
+    help="This field cannot be left blank!"
+)
+_request_parser.add_argument('done',
+    type=str,
+    required=True,
+    help="This field cannot be left blank!"
+)
+
+
 class Request(Resource):
 
     def get(self, id):
@@ -10,6 +38,23 @@ class Request(Resource):
             return request.json()
         return {'message': 'request not found'}, 404
 
+    def put(self,id):
+        data = _request_parser.parse_args() #Validacao das condicoes de entrada
+
+        request = RequestModel.find_by_id(id)
+        
+        if request:            
+            request.name = data['name']
+            request.address = data['address']
+            request.author = data['author']
+            request.accepted = data['accepted']
+            request.done = data['done']
+        else:
+            return {'message': 'Request not found'}, 404
+        
+        request.save_to_db()
+        return request.json()
+
     def delete(self, id):
         request = RequestModel.find_by_name(id)
         if request:
@@ -17,39 +62,13 @@ class Request(Resource):
 
         return {'message': 'Request deleted'}
 
-class NewRequest(Resource):
 
-    parser = reqparse.RequestParser()
-    parser.add_argument('name',
-        type=str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
-    parser.add_argument('address',
-        type=str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
-    parser.add_argument('author',
-        type=str,
-        required=True,
-        help="Every request needs a request_id"
-    )
-    parser.add_argument('accepted',
-        type=str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
-    parser.add_argument('done',
-        type=str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
+class NewRequest(Resource):
 
     @jwt_required
     def post(self):
 
-        data = NewRequest.parser.parse_args() #Validacao das condicoes de entrada
+        data = _request_parser.parse_args() #Validacao das condicoes de entrada
 
         if RequestModel.find_by_name(data['name']):
             return {'message': "A Request with name '{}' already exists".format(data['name'])}, 400
